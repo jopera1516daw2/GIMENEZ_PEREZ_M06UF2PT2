f5bbe976b17d945df8954360d7d5891340ccc1b0
jopera1516daw2 <jopera1516daw2@gmail.com>
2017-01-19 21:07:09 +0100

    update with connection with db

--- GIMENEZ_PEREZ_M06UF2PT2/GIMENEZ_PEREZ_M06UF2PT2/Program.cs
+++ GIMENEZ_PEREZ_M06UF2PT2/GIMENEZ_PEREZ_M06UF2PT2/Program.cs
@@ -8,15 +8,24 @@ namespace GIMENEZ_PEREZ_M06UF2PT2
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            var db = new DB();
+            var pet = new TableProducte { Id_producte = 2, Producte = "Harambe's soul", Preu = 100 };
+            db.TableProductes.Add(pet);
+            db.SaveChanges();
+
+            var productes = db.TableProductes;
+            foreach (var p in productes)
+            {
+                Console.WriteLine(p.Producte);
+            }
+            Console.Read();
         }
     }
 }
---
+++ GIMENEZ_PEREZ_M06UF2PT2/GIMENEZ_PEREZ_M06UF2PT2/TableProducte.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace GIMENEZ_PEREZ_M06UF2PT2
+{
+    [Table("productes", Schema = "public")]
+    public class TableProducte
+    {
+        [Key]
+        [Column("id_producte")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id_producte { get; set; }
+        [Column("producte")]
+        public string Producte { get; set; }
+        [Column("preu")]
+        public int Preu { get; set; }
+    }
+
+    public class DB : DbContext
+    {
+        public DB() : base(nameOrConnectionString: "EmpresaEntities") { }
+        public DbSet<TableProducte> TableProductes { get; set; }
+    }
+
+}
